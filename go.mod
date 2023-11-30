4a687446b71a62a7675b121b96ee164d48dd8215
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-12-01 02:06:21 +0900

    read exif from sample.jpeg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/youpong/read-exif
 
 go 1.21.4
+
+require github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-// "github.com/rwcarlsen/goexif/exif"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/rwcarlsen/goexif/exif"
+)
+
+func ExampleRead() {
+	fname := "sample.jpeg"
+
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tm, _ := x.DateTime()
+	fmt.Println("Taken: ", tm)
+}
+
+func main() {
+	ExampleRead()
+}
